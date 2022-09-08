38565a9392ac1ecfa10595bdc292dc6d262f2a23
unknown <alyssoncolombo@outlook.com>
2022-09-08 15:52:26 -0300

    feat: Create table pivot in table order

--- products/serializers.py
+++ products/serializers.py
@@ -38,23 +38,6 @@ class ProductSerializer(serializers.ModelSerializer):
   category = CategorySerializer()
   ingredients = IngredientsSerializer(many=True)
   
-  def create(self, validated_data):  
-    import ipdb
-    ipdb.set_trace()  
-    category_serializer = validated_data.pop('category')
-    ingredients_list = validated_data.pop('ingredients')
-    
-    category_instance,_ = Category.objects.get_or_create(**category_serializer)
-    
-    product: Product = Product.objects.create(**validated_data, category=category_instance)
-    
-    for ingredient in ingredients_list:
-      instance_ingredient,_ = Ingredient.objects.get_or_create(**ingredient)
-      product.ingredients.add(instance_ingredient)
-      
-    product.save() 
-          
-    return product
 
     
   
