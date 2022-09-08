e91d001d40e2d7ca49b50c9d398296e57db3c17e
kennedybm <kennedy_muniz@hotmail.com>
2022-09-08 14:32:35 -0300

    Feat: created utils with customized mixin

--- orders/serializers.py
+++ orders/serializers.py
@@ -3,15 +3,10 @@ from datetime import datetime
 from .models import Order
 
 class OrderSerliazer(serializers.ModelSerializer):
-    withdrawal_date = serializers.SerializerMethodField()
 
     class Meta:
         model = Order
-        fields = ["id", "comment", "is_finished", "total"]
-        extra_kwargs = {"total": {"read_only": True}, "withdrawal_date": {"read_only": True}}
+        fields = ["id", "comment", "withdrawal_date", "is_finished", "total"]
+        extra_kwargs = {"total": {"read_only": True}}
 
 
-    def get_withdrawal_date(self, obj: Order) -> str:
-        actual_datetime = datetime.now()
-        date_time = actual_datetime.strftime("%d/%m/%Y %H:%M:%S")
-        return date_time
