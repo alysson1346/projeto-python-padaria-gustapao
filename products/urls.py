d5a9e14a1d4d6795f332b5cacf87202d90f4a375
João Francisco Guarda <95258409+joaofranciscoguarda@users.noreply.github.com>
2022-09-09 10:15:30 -0300

    Revert "Fix/minor fixes"

--- products/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from products import views
-
-urlpatterns = [
-    path("", views.ProductListCreateView.as_view()),
-    path("<pk>", views.ProductDetailsView.as_view()),
-]
