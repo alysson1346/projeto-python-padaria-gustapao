824433734ed6395de0fefcd0a229e3c1fffe8acb
kennedybm <kennedy_muniz@hotmail.com>
2022-09-12 18:06:21 -0300

    Feat: created order_status field, with 3 choices, serializzer, view and permission, only admin or staff can update order status

--- orders/permissions.py
+++ orders/permissions.py
@@ -4,3 +4,7 @@ from rest_framework import permissions
 class IsOwnerOrStaffOrAdmin(permissions.IsAuthenticated):
   def has_object_permission(self, request, view, obj):
     return request.user == obj
+
+class IsAdminOrStaff(permissions.IsAuthenticated):
+  def has_permission(self, request, view):
+    return request.user.is_superuser or request.user.is_staff
--- orders/views.py
+++ orders/views.py
@@ -1,15 +1,16 @@
 from rest_framework import generics
+from utils.mixins import SerializerByMethodMixin
 from rest_framework.views import Response, status
 from accounts.models import Account
 
 from orders.models import Order
-from orders.permissions import IsOwnerOrStaffOrAdmin
-from orders.serializers import OrderSerializer
+from orders.permissions import IsOwnerOrStaffOrAdmin, IsAdminOrStaff
+from orders.serializers import OrderSerializer, OrderStatusSerializer
 
 
 class OrderView(generics.ListCreateAPIView):
     queryset = Order.objects.all()
-    permission_classes = [IsOwnerOrStaffOrAdmin]
+    # permission_classes = [IsOwnerOrStaffOrAdmin]
     serializer_class = OrderSerializer
 
     def perform_create(self, serializer):
@@ -17,6 +18,15 @@ class OrderView(generics.ListCreateAPIView):
 
 
 class OrderDetailView(generics.RetrieveUpdateDestroyAPIView):
-  queryset = Order.objects.all()
-  permission_classes = [IsOwnerOrStaffOrAdmin]
-  serializer_class = OrderSerializer
+    queryset = Order.objects.all()
+    # permission_classes = [IsOwnerOrStaffOrAdmin]
+    serializer_class = OrderSerializer
+
+class OrderStatusView(SerializerByMethodMixin, generics.RetrieveUpdateAPIView):
+
+    permission_classes = [IsAdminOrStaff]
+
+    queryset = Order.objects.all()
+    serializer_map = {
+      'PATCH': OrderStatusSerializer
+    }
