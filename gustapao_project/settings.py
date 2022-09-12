8c7c712b0696eb8788b62f29787595462a91c17c
João Francisco <joaofrancisco.guarda@gmail.com>
2022-09-12 12:06:08 -0300

    heroku config with docker
    Signed-off-by: João Francisco <joaofrancisco.guarda@gmail.com>

--- gustapao_project/settings.py
+++ gustapao_project/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0','localhost', 'tcmgustapao.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0','localhost', 'tcmgustapao.herokuapp.com', 'tcmgustapaodocker.herokuapp.com']
 
 
 # Application definition
